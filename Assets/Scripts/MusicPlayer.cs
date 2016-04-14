369e034317a08ee92735c6b7a17a1d7b4f20fab9
Eric King <e.king117@gmail.com>
2016-04-13 21:35:14 -0400

    Music only starts once, but object still instantiated multiple times

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -2,14 +2,21 @@
 using System.Collections;
 
 public class MusicPlayer : MonoBehaviour {
-
+	
+	public static bool instantiated = false;
 	// Use this for initialization
 	void Start () {
+		AudioSource audio = GetComponent<AudioSource>();
 		GameObject.DontDestroyOnLoad(gameObject);
+		if (!instantiated){
+			audio.Play();
+			instantiated = true;
+		}
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 }
