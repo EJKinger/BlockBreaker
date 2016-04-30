503b3c6da8bea56387eb3c320dee29489f7f7e22
Eric King <e.king117@gmail.com>
2016-04-30 14:01:57 -0400

    Paddle now maps to mouse position

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float mousePosInBlocks = Mathf.Clamp((Input.mousePosition.x / Screen.width * 16) - 8, -7.5f, 7.5f);
+		
+		this.transform.position = new Vector3(mousePosInBlocks, this.transform.position.y, 1f);
+	}
+}
