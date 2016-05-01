c1907fe4f8b3d66c2d3889da71cd3f400b59d1c2
Eric King <e.king117@gmail.com>
2016-05-01 16:30:39 -0400

    Modified gravity scale, looped music

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -19,7 +19,7 @@ public class Ball : MonoBehaviour {
 		if (!hasStarted) {
 			if (Input.GetMouseButtonDown(0)){
 				hasStarted = true;
-				this.rigidbody2D.velocity = new Vector2 (0f, 20f);
+				this.rigidbody2D.velocity = new Vector2 (2f, 10f);
 			}
 			this.transform.position = paddle.transform.position + paddleToBallVector;
 		}
