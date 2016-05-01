b9b325494e3545ec3172a3b7eb37d60ad5293550
Eric King <e.king117@gmail.com>
2016-05-01 15:39:10 -0400

    Added walls and ceiling colliders

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -10,7 +10,7 @@ public class Paddle : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		float mousePosInBlocks = Mathf.Clamp((Input.mousePosition.x / Screen.width * 16) - 8, -7.5f, 7.5f);
+		float mousePosInBlocks = Mathf.Clamp((Input.mousePosition.x / Screen.width * 16), 0.5f, 15.5f);
 		
 		this.transform.position = new Vector3(mousePosInBlocks, this.transform.position.y, 1f);
 	}
