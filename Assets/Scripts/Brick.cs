ad392e2a6a42cc5a948de400d66e48bedda77873
Eric King <e.king117@gmail.com>
2016-05-08 13:23:58 -0400

    added simulated win condition to advance to next level

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -5,9 +5,11 @@ public class Brick : MonoBehaviour {
 
 	public int maxHits;
 	private int hits;
+	private LevelManager levelManager;
 
 	// Use this for initialization
 	void Start () {
+		levelManager = GameObject.FindObjectOfType<LevelManager>();
 		hits = 0;
 	}
 	
@@ -18,5 +20,11 @@ public class Brick : MonoBehaviour {
 	
 	void OnCollisionEnter2D () {
 		hits++;
+		simulateWin();
+  	}
+  	
+  	// TODO Remove this method when actual win conditions are completed
+  	void simulateWin() {
+  		levelManager.LoadNextLevel();
   	}
 }
