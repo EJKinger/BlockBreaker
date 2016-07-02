d2a76997404bd6921a2516025d6349d962b93550
Eric King <e.king117@gmail.com>
2016-07-02 18:12:44 -0400

    Added auto-play for testing

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -3,15 +3,29 @@ using System.Collections;
 
 public class Paddle : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+	public bool AutoPlay = false;
+	private Ball ball;
+	// Update is called once per frame
 	
+	void Start () {
+		ball = GameObject.FindObjectsOfType<Ball>()[0];
+	}
+	void Update () {
+		
+		if (!AutoPlay) {
+			MoveWithMouse();
+		} else {
+			MoveAutomatically();
+		}
+		
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	void MoveWithMouse () {
 		float mousePosInBlocks = Mathf.Clamp((Input.mousePosition.x / Screen.width * 16), 0.5f, 15.5f);
-		
 		this.transform.position = new Vector3(mousePosInBlocks, this.transform.position.y, 1f);
+	}
+	
+	void MoveAutomatically () {
+		this.transform.position = new Vector3(ball.transform.position.x, this.transform.position.y, 1f);
 	}
 }
